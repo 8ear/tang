06a6dc83952e5c365ed79d6c1bb4190844c062e2
Max H <10329648+8ear@users.noreply.github.com>
2021-09-26 16:54:20 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
+set -x
+
 ## Prepare Variables
 TANG_CACHE_PATH=${TANG_CACHE_PATH:-"/var/cache/tang"}
 
-if [ "$1" == "tangd-rotate-keys" ] ; then
+# Check if CMD is set
+if [[ "$1" == "tangd-rotate-keys" ]] ; then
   "Start rotating keys..."
   exec tangd-rotate-keys -d "$TANG_CACHE_PATH"
   "End rotating keys..."
 fi
 
-if [ "$1" == "bash" ] || [ "$1" == "sh" ] ; then
+# Check if CMD is set
+if [[ "$1" == "bash" ]] || [[ "$1" == "sh" ]] ; then
   exec bash
+  exit 0
 fi
 
 ## Prepare Log
@@ -17,7 +22,11 @@ echo "=================================="
 echo "`date`:"
 
 # Check if keys already exists, if not create one
-[ -f "$TANG_CACHE_PATH/*.jwk" ] || ( tangd-rotate-keys -d "$TANG_CACHE_PATH" && echo "Rotate keys...")
+if [[ -f "$TANG_CACHE_PATH/*.jwk" ]] ; then
+  echo "No keys exists, generate some..."
+  tangd-rotate-keys -d "$TANG_CACHE_PATH"
+  echo "... done"
+fi
 
 ## Start Application
 socat tcp-l:80,reuseaddr,fork exec:"tangd $TANG_CACHE_PATH"
